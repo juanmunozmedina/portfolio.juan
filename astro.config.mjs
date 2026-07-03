dd9a62ebc61d9b06f43b5e309073b4cea0fa359f
Juan <juanmunozmedina96@gmail.com>
2026-07-03 18:53:23 +0200

    UPDATE dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import robotsTxt from "astro-robots-txt";
+import tailwindcss from "@tailwindcss/vite";
 
-import robotsTxt from "astro-robots-txt"
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://juanmunozmedina.vercel.app/'
-})
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [robotsTxt()],
+  site: "https://juanmunozmedina.vercel.app/",
+});
